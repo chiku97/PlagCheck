cab7e14a62b445a9404351f1ff3914870278e111
Uttam Kumar <mahtouttamkumar01@gmail.com>
2025-05-14 12:52:40 +0530

    changed the folder path

--- utils/gitUtils.js
+++ utils/gitUtils.js
@@ -1,12 +1,10 @@
 import simpleGit from 'simple-git';
 import fs from 'fs-extra';
 import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 export async function cloneRepos(repoA, repoB) {
-  const baseDir = path.resolve(__dirname, '..', 'repos');
+    const baseDir = path.join('/tmp', 'repos');
   const repoAPath = path.join(baseDir, 'repoA');
   const repoBPath = path.join(baseDir, 'repoB');
 
